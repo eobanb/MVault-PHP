12740ec88fa713add12e9b81a3176ca572c7adac
Eoban Binder <ebinder@indiana.edu>
2015-11-20 11:44:09 -0500

    Creating functions.php

---
+++ mvphp-functions.php
@@ -0,0 +1,24 @@
+<?php
+
+include('./httpful.phar'); // HTTPful library
+
+function MVwriteRecord($uri) {
+	global $username, $password, $json_record;
+	$response = \Httpful\Request::put($uri) // PUT for creating
+	->sendsJson() // JSON
+    ->authenticateWith($username, $password)
+    ->body($json_record) 
+    ->send();
+    return $response;
+}
+
+function MVgetRecord($uri) {
+	global $username, $password;
+	$response = \Httpful\Request::get($uri) // GET for retrieving
+    	->authenticateWith($username, $password)
+    	->send();
+	return $response;
+	return $status = 'record retrieved';
+}
+
+?>
